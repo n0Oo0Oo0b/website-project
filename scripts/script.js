a9329ef56d8e890b186075125fd57adfdd2d3391
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2022-06-06 14:53:21 +0800

    Parallax example

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,13 @@
 $(() => {
+    let $parallax = $('.parallax')
+    for (const i of $parallax) {
+        $i = $(i)
+        console.log(i.offsetTop)
+        if ($i.css('--offset').includes('auto')) {
+            $i.css('--offset', i.offsetTop + 'px')
+        }
+    }
+
     document.addEventListener('scroll', () => {
         $(document.documentElement).css('--page-scroll', window.scrollY + 'px')
     }, {passive:true})
