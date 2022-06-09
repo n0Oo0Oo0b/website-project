9f7d1050b44d8fb3c99b2a872a2106a87d5bbe75
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2022-06-09 09:12:36 +0800

    Created parallax #1

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,22 @@
 $(() => {
+    let $repeats = $('[repeat]')
+    for (const i of $repeats) {
+        $i = $(i)
+        count = $i.attr('repeat')
+        $i.removeAttr('repeat')
+        $i.css('--id', count-1)
+        for (var j=0; j<count-1; j++) {
+            $e = $i.clone().css('--id', j)
+            $i.after($e)
+        }
+    }
+
     let $parallax = $('.parallax')
     for (const i of $parallax) {
         $i = $(i)
-        console.log(i.offsetTop)
+        console.log($i)
         if ($i.css('--offset').includes('auto')) {
-            $i.css('--offset', i.offsetTop + 'px')
+            $i.css('--offset', i.getBoundingClientRect().top + 'px')
         }
     }
 
