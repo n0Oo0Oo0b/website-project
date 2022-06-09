3a522d00b471206f0619a889837e98792570e4ab
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2022-06-09 14:09:56 +0800

    Updated toc.js to new layout

--- scripts/toc.js
+++ scripts/toc.js
@@ -1,25 +1,10 @@
-// toc = table of contents
-let headerName;
-let $toc;
-
-let idName = (n) => {
-    return n.toLowerCase().replace(' ', '-');
-};
-
 $(() => {
-    $toc = $('#toc-list');
-    
-    let $headers = $('.article-heading');
+    let $toc = $('#page-nav > ul');
+    let $headers = $('article section');
     for (var i=0; i<$headers.length; i++) {
-        headerName = $headers[i].textContent;
-        
-        // Pointer 80px above heading to compensate for sticky header
-        $(`<div class="heading-pointer" id="${idName(headerName)}">`)
-            .prependTo($headers[i]);
-        
-        // Add link to toc list
-        $('<li class="toc-item">')
-            .append($(`<a class="nav-link" href="#${idName(headerName)}">${headerName}</a>`))
-            .appendTo($toc);
+        $i = $($headers[i]);
+        heading = $i.find('h2')[0].innerText;
+        id = $i[0].id;
+        $toc.append($(`<li><a href="#${id}">${heading}</a></li>`));
     }
 });
