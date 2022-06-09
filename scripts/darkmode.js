5e8804cb1e86bf5b9674e310f8e64d54b1df9439
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2022-06-09 17:07:52 +0800

    Dark mode cookies

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,8 +1,25 @@
 $(() => {
+    let theme = null;
+    let c = decodeURIComponent(document.cookie).split(';');
+    c.forEach((i) => {
+        let s = i.split('=')
+        if (s[0] === 'theme') {
+            theme = s[1]
+        }
+    })
     $root = $(document.documentElement)
-    $root.attr('data-theme', window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark')
+    if (theme === null) {
+        theme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark'
+    }
+    $root.attr('data-theme', theme)
 
     toggleDark = function() {
-        $root.attr('data-theme', $root.attr('data-theme') === 'light' ? 'dark' : 'light')
+        let theme = $root.attr('data-theme') === 'light' ? 'dark' : 'light'
+        $root.attr('data-theme', theme)
+        document.cookie = `theme=${theme}`;
+    }
+
+    clearThemePreference = function() {
+        document.cookie = "theme=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     }
 })
