3d3ebe7031862fd2034608282a81bac9760058f3
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2022-06-14 10:34:14 +0800

    ;

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -2,24 +2,20 @@ $(() => {
     let theme = null;
     let c = decodeURIComponent(document.cookie).split(';');
     c.forEach((i) => {
-        let s = i.split('=')
+        let s = i.split('=');
         if (s[0] === 'theme') {
-            theme = s[1]
+            theme = s[1];
         }
     })
-    $root = $(document.documentElement)
+    $root = $(document.documentElement);
     if (theme === null) {
-        theme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark'
+        theme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark';
     }
-    $root.attr('data-theme', theme)
+    $root.attr('data-theme', theme);
 
     toggleDark = function() {
-        let theme = $root.attr('data-theme') === 'light' ? 'dark' : 'light'
-        $root.attr('data-theme', theme)
+        let theme = $root.attr('data-theme') === 'light' ? 'dark' : 'light';
+        $root.attr('data-theme', theme);
         document.cookie = `theme=${theme}; SameSite=Lax; path=/;`;
     }
-
-    clearThemePreference = function() {
-        document.cookie = "theme=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    }
-})
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,27 @@
 $(() => {
-    let $repeats = $('[repeat]')
+    let $repeats = $('[repeat]');
     for (const i of $repeats) {
-        $i = $(i)
-        count = $i.attr('repeat')
-        $i.removeAttr('repeat')
-        $i.css('--id', count-1)
+        $i = $(i);
+        count = $i.attr('repeat');
+        $i.removeAttr('repeat');
+        $i.css('--id', count-1);
         for (var j=0; j<count-1; j++) {
-            $e = $i.clone().css('--id', j)
-            $i.after($e)
+            $e = $i.clone().css('--id', j);
+            $i.after($e);
         }
     }
 
-    let $parallax = $('.parallax')
+    let $parallax = $('.parallax');
     for (let i=0; i<$parallax.length; i++) {
-        $i = $($parallax[i])
-        console.log($i)
+        $i = $($parallax[i]);
+        console.log($i);
         if ($i.css('--offset').includes('auto')) {
-            $i.css('--offset', ($i.parent()[0].getBoundingClientRect().top - 50) + 'px')
+            $i.css('--offset', ($i.parent()[0].getBoundingClientRect().top - 50) + 'px');
         }
-        $i.css('z-index', i)
+        $i.css('z-index', i);
     }
 
     document.addEventListener('scroll', () => {
-        $(document.documentElement).css('--page-scroll', window.scrollY + 'px')
-    }, {passive:true})
-})
+        $(document.documentElement).css('--page-scroll', window.scrollY + 'px');
+    }, {passive:true});
+});
